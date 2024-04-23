d9bd92f6afac6d86d5d071e076cfae41aa0a71e3
George <georgejoj28@gmail.com>
2024-04-23 08:45:49 +0300

    add phpmyadmin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
 
 Route::get('/', function () {
     return view('welcome');
